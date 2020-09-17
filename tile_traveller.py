6aaae422d5fda8c2d66e4f5385576c25f4c5415f
Nói Sigurðarson <noisigurdarson@MacBook-Pro.lan>
2020-09-17 13:03:41 +0000

    functions made

---
+++ tile_traveller.py
@@ -0,0 +1,20 @@
+# Git Repo: https://github.com/palmigudmunds/TileTraveler
+
+x = 1
+y = 1
+
+def moveUp(y):
+    y += 1
+    return y
+
+def moveDown(y):
+    y -= 1
+    return y
+
+def moveLeft(x):
+    x -= 1
+    return x
+
+def moveRight(x):
+    x += 1
+    return x
