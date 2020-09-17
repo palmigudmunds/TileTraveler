88e7bf7af061e89dc8f831d01f5cbb567dfb8806
Pálmi Guðmundsson <PalmiGudmunds@Palmis-MBP.lan>
2020-09-17 13:06:43 +0000

    more functions made

--- tile_traveller.py
+++ tile_traveller.py
@@ -18,3 +18,31 @@ def moveLeft(x):
 def moveRight(x):
     x += 1
     return x
+
+def north(user_input,x,y):
+    if user_input == 'n' or user_input == 'N':
+        y = moveUp(y)
+        return True,x,y
+    else:
+        return False,x,y
+        
+def south(user_input,x,y):
+    if user_input == 's' or user_input == 'S':
+        y = moveDown(y)
+        return True,x,y
+    else:
+        return False,x,y
+
+def east(user_input,x,y):
+    if user_input == 'e' or user_input == 'E':
+        x = moveRight(x)
+        return True,x,y
+    else:
+        return False,x,y
+
+def west(user_input,x,y):
+    if user_input == 'w' or user_input == 'W':
+        x = moveLeft(x)
+        return True,x,y
+    else:
+        return False,x,y
