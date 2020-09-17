7a041a36f116fba612256dcdae84cae34a9f1a16
Pálmi Guðmundsson <PalmiGudmunds@Palmis-MBP.lan>
2020-09-17 14:05:02 +0000

    fullklarað verkefni

--- tile_traveller.py
+++ tile_traveller.py
@@ -46,8 +46,11 @@ def west(user_input,x,y):
         return True,x,y
     else:
         return False,x,y
-       
-if x==1 and y==1:
+gameNotOver = True
+
+while gameNotOver:
+
+    if x==1 and y==1:
         print("You can travel: (N)orth.")
         user_input = input("Direction: ")
         valid,new_x,new_y = north(user_input,x,y)
@@ -56,3 +59,112 @@ if x==1 and y==1:
         else:
             x=new_x
             y=new_y
+
+    elif x==1 and y==2:
+        print("You can travel: (N)orth or (E)ast or (S)outh.")
+        user_input = input("Direction: ")
+        if user_input == 's' or user_input == 'S':
+            valid,new_x,new_y = south(user_input,x,y)
+        elif user_input == 'n' or user_input == 'N':
+            valid,new_x,new_y = north(user_input,x,y)
+        elif user_input == 'e' or user_input == 'E':
+            valid,new_x,new_y = east(user_input,x,y)
+        else:
+            valid = False
+        if valid == False:
+            print("Not a valid direction!")
+        else:
+            x=new_x
+            y=new_y       
+        
+    elif x==2 and y==2:
+        print("You can travel: (S)outh or (W)est.")
+        user_input = input("Direction: ")
+        if user_input == 's' or user_input == 'S':
+            valid,new_x,new_y = south(user_input,x,y)
+        elif user_input == 'w' or user_input == 'W':
+            valid,new_x,new_y = west(user_input,x,y)
+        else:
+            valid = False
+        if valid == False:
+            print("Not a valid direction!")
+        else:
+            x=new_x
+            y=new_y
+
+    elif x==2 and y==1:
+        print("You can travel: (N)orth.")
+        user_input = input("Direction: ")
+        if user_input == 'n' or user_input == 'N':
+            valid,new_x,new_y = north(user_input,x,y)
+        else:
+            valid = False
+        if valid == False:
+            print("Not a valid direction!")
+        else:
+            x=new_x
+            y=new_y
+
+    elif x==1 and y==3:
+        print("You can travel: (E)ast or (S)outh.")
+        user_input = input("Direction: ")
+        if user_input == 's' or user_input == 'S':
+            valid,new_x,new_y = south(user_input,x,y)
+        elif user_input == 'e' or user_input == 'E':
+            valid,new_x,new_y = east(user_input,x,y)
+        else:
+            valid = False
+        if valid == False:
+            print("Not a valid direction!")
+        else:
+            x=new_x
+            y=new_y
+
+    elif x==2 and y==3:
+        print("You can travel: (E)ast or (W)est.")
+        user_input = input("Direction: ")
+        if user_input == 'e' or user_input == 'E':
+            valid,new_x,new_y = east(user_input,x,y)
+        elif user_input == 'w' or user_input == 'W':
+            valid,new_x,new_y = west(user_input,x,y)
+        else:
+            valid = False
+        if valid == False:
+            print("Not a valid direction!")
+        else:
+            x=new_x
+            y=new_y
+
+    elif x==3 and y==3:
+        print("You can travel: (S)outh or (W)est.")
+        user_input = input("Direction: ")
+        if user_input == 's' or user_input == 'S':
+            valid,new_x,new_y = south(user_input,x,y)
+        elif user_input == 'w' or user_input == 'W':
+            valid,new_x,new_y = west(user_input,x,y)
+        else:
+            valid = False
+        if valid == False:
+            print("Not a valid direction!")
+        else:
+            x=new_x
+            y=new_y
+
+    elif x==3 and y==2:
+        print("You can travel: (N)orth or (S)outh.")
+        user_input = input("Direction: ")
+        if user_input == 'n' or user_input == 'N':
+            valid,new_x,new_y = north(user_input,x,y)
+        elif user_input == 's' or user_input == 'S':
+            valid,new_x,new_y = south(user_input,x,y)
+        else:
+            valid = False
+        if valid == False:
+            print("Not a valid direction!")
+        else:
+            x=new_x
+            y=new_y
+
+    elif x==3 and y==1:
+        print("Victory!")
+        gameNotOver = False
